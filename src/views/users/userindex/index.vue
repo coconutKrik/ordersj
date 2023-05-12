b421915dbbbf1f2c1be0c6ad3b36d1f95659938f
xiangxin2001 <2868308648@qq.com>
2023-05-12 20:58:20 +0800

    主页图片

--- src/views/users/userindex/index.vue
+++ src/views/users/userindex/index.vue
@@ -1,5 +1,5 @@
 <template>
- <div>
+ <div class="indexpage">
     <div class="header">
         <userheader></userheader>
     </div>
@@ -240,6 +240,7 @@ import userheader from './../userheader.vue'
 </script>
 
 <style lang='less' scoped>
+
     .header{
         width: 100%;
         height: 20%;
@@ -253,15 +254,17 @@ import userheader from './../userheader.vue'
         position: relative;
     }
     .footer{
+        background-image: url('../../../assets/southeast.jpg');
         width: 100%;
         height: auto;
         display: inline-block;
         position: relative;
         border-top:2px solid #fe000074;
 	    margin:30px 0;
+        color:white;
     }
 .foot_link{text-align:center;margin-top:30px;}
-.foot_link a,.foot_link span{color:#4e4e4e;}
+.foot_link a,.foot_link span{color:#e6e6e6;}
 .foot_link a:hover{color:#f821ff}
 .foot_link span{padding:0 10px}
 .footer p{text-align:center; margin-top:10px;}
